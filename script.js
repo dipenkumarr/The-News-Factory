086fa35b706e3b8f5aede03fb05d91a2fafb10dc
dipen <dipenbachani@gmail.com>
2023-08-10 17:16:53 +0400

    first comming before gp

---
+++ script.js
@@ -0,0 +1,70 @@
+const API_KEY = "3cba9ee7d2ff4ad99a80fa558026915a";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+async function fetchNews(query) {
+  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+  const data = await res.json();
+  bindData(data.articles);
+}
+
+function bindData(articles) {
+  const cardsContainer = document.getElementById("cards-container");
+  const newsCardTemplate = document.getElementById("template-news-card");
+
+  cardsContainer.innerHTML = "";
+
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+    const CardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(CardClone, article);
+    cardsContainer.appendChild(CardClone);
+  });
+}
+
+function fillDataInCard(CardClone, article) {
+  const newsImg = CardClone.querySelector("#news-img");
+  const newsTitle = CardClone.querySelector("#news-title");
+  const newsSource = CardClone.querySelector("#news-source");
+  const newsDesc = CardClone.querySelector("#news-desc");
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+  CardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank");
+  });
+}
+
+let curSelectedNav = null;
+
+function onNavItemClick(id) {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = navItem;
+  curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+  const query = searchText.value;
+  if (!query) return;
+  fetchNews(query);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = null;
+});
+
+function reload() {
+  window.location.reload();
+}
